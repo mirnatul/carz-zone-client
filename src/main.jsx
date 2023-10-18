ba39e8def8e48941858773f4ada832eb99f78f18
Li Pon <mirnatul@gmail.com>
2023-10-18 17:46:21 +0600

    updated for localhost

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from '../pages/shared/Footer/Footer';
-import NavBar from '../pages/shared/NavBar/NavBar';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import router from './Routes/routes'
+import AuthProvider from './provider/AuthProvider';
+// ..
+AOS.init();
 
-const Main = () => {
-    return (
-        <div className='min-h-[100vh] flex flex-col'>
-            <NavBar></NavBar>
-            <div className='bg-[#fbfded]'>
-                <Outlet></Outlet>
-            </div>
-            <Footer></Footer>
-        </div>
-    );
-};
 
-export default Main;
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
+    </React.StrictMode>,
+)
